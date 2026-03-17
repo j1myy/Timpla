5a312dff6ea6a6ab7497a8c6f862077521bb1305
Jimwel Mañibo <kji.wo316@gmail.com>
2026-03-17 20:47:25 +0800

    config: Conditionally set Vite base path to support Vercel deployments.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/Timpla/',  // <-- ADD THIS LINE HERE
+    base: process.env.VERCEL ? '/' : '/Timpla/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
